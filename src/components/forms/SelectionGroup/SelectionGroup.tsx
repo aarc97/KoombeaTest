1bc5cd64eb66d14879322f6242d17d67c1c65a38
Andris Ramirez <46964876+aarc97@users.noreply.github.com>
2022-02-07 17:07:13 -0400

    adding SelectionGroup component

---
+++ src/components/forms/SelectionGroup/SelectionGroup.tsx
@@ -0,0 +1,52 @@
+import {map} from 'lodash';
+import React, {useCallback, useState, ComponentType, FC} from 'react';
+import {ScrollView} from 'react-native';
+
+interface ISelectionGroup {
+  values: any[];
+  onPress: (item: any, i: number) => void;
+  CustomComponent: ComponentType<any>;
+  horizontal?: boolean;
+}
+
+const SelectionGroup: FC<ISelectionGroup> = ({
+  values,
+  CustomComponent,
+  horizontal,
+  onPress,
+}) => {
+  const [currentSelectedIndex, setCurrentSelectedItem] = useState<number>(0);
+
+  const renderItem = useCallback(
+    (item: any, idx: number) => {
+      const isSelected = currentSelectedIndex === idx;
+      const onPressed = () => {
+        onPress(item, idx);
+        setCurrentSelectedItem(idx);
+      };
+
+      return (
+        <CustomComponent
+          key={`${idx}`}
+          isSelected={isSelected}
+          item={item}
+          onPress={onPressed}
+        />
+      );
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [currentSelectedIndex],
+  );
+
+  if (!CustomComponent) {
+    throw new Error('You must wrap a custom component');
+  }
+
+  return (
+    <ScrollView horizontal={horizontal}>
+      {map(values || [], renderItem)}
+    </ScrollView>
+  );
+};
+
+export default SelectionGroup;
