f06852061b74da9ac66f1b6f30f72712de7c7acb
Andris Ramirez <46964876+aarc97@users.noreply.github.com>
2022-02-07 12:57:52 -0400

    removing unused lodash imports

--- src/libraries/lodash.ts
+++ src/libraries/lodash.ts
@@ -1,26 +1,19 @@
 import capitalize from 'lodash/capitalize';
-import chain from 'lodash/chain';
 import compact from 'lodash/compact';
-import countBy from 'lodash/countBy';
-import differenceBy from 'lodash/differenceBy';
 import every from 'lodash/every';
 import filter from 'lodash/filter';
 import find from 'lodash/find';
 import findIndex from 'lodash/findIndex';
 import findLastIndex from 'lodash/findLastIndex';
-import flow from 'lodash/flow';
 import groupBy from 'lodash/groupBy';
 import isArray from 'lodash/isArray';
 import isEmpty from 'lodash/isEmpty';
 import isNull from 'lodash/isNull';
 import isUndefined from 'lodash/isUndefined';
-import isEqual from 'lodash/isEqual';
 import isFunction from 'lodash/isFunction';
 import map from 'lodash/map';
-import maxBy from 'lodash/maxBy';
 import memoize from 'lodash/memoize';
 import orderBy from 'lodash/orderBy';
-import padStart from 'lodash/padStart';
 import sortBy from 'lodash/sortBy';
 import sumBy from 'lodash/sumBy';
 import toNumber from 'lodash/toNumber';
@@ -28,23 +21,15 @@ import toPairs from 'lodash/toPairs';
 import toString from 'lodash/toString';
 import trim from 'lodash/trim';
 import split from 'lodash/split';
-import zipObject from 'lodash/zipObject';
 import isString from 'lodash/isString';
-import uniqWith from 'lodash/uniqWith';
 
 export {
-  uniqWith,
   isString,
   isNull,
   isUndefined,
-  countBy,
   isFunction,
-  flow,
   every,
   capitalize,
-  zipObject,
-  chain,
-  differenceBy,
   filter,
   groupBy,
   toNumber,
@@ -56,10 +41,7 @@ export {
   toPairs,
   toString,
   find,
-  isEqual,
   findIndex,
-  maxBy,
-  padStart,
   findLastIndex,
   compact,
   trim,
