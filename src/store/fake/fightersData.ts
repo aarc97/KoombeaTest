6fde3194492d5bd348c4ff7671c993d47fc8f76c
Andris Ramirez <46964876+aarc97@users.noreply.github.com>
2022-02-07 21:28:16 -0400

    testing fighter utils methods

---
+++ src/store/fake/fightersData.ts
@@ -0,0 +1,56 @@
+export const fightersData = [
+  {
+    objectID: 'f56aa7cd-6c66-4752-bbb7-36fee86bd27d',
+    name: 'Kirby',
+    universe: 'Kirby',
+    price: '762',
+    popular: true,
+    rate: 3,
+    downloads: '63630',
+    description:
+      'Kirby (カービィ, Kirby) is a playable character in Super Smash Bros. Ultimate. Like all veterans, he was confirmed on June 12th, 2018. Kirby is classified as Fighter #06. Kirby is voiced in all regions once again by Makiko Ōmoto, using a combination of new voice clips and ones recycled from Super Smash Bros. 4.',
+    created_at: 'Sat Jan 29 2022 01:50:52 GMT-0500 (-05)',
+    imageURL: 'https://myapp.koombea.com/smash/19.png',
+  },
+
+  {
+    objectID: 'f56aa7cd-6c66-4752-bbb7-36fee86bd27d',
+    name: 'Chrom',
+    universe: 'Fire Emblem',
+    price: '272',
+    popular: true,
+    rate: 2,
+    downloads: '11602',
+    description:
+      "Chrom (クロム, Chrom) is a playable character in Super Smash Bros. Ultimate. He was announced as a newcomer alongside Simon, Richter, Dark Samus, and King K. Rool in the Nintendo Direct on August 8th, 2018. Chrom is Roy's Echo Fighter, thus being classified as Fighter #25ε, though his lack of sweetspots makes him parallel Roy the same way Lucina parallels Marth. In addition to his role as a playable character, Chrom remains a part of Robin's final smash Pair Up. Matthew Mercer and Tomokazu Sugita, who voiced Chrom in the English and Japanese versions of Fire Emblem Awakening respectively, reprise their roles in the international and Japanese versions of Ultimate. Both voice actors initially revealed their roles on their Twitter accounts [1] [2].",
+    created_at: 'Sat Jan 29 2022 01:50:52 GMT-0500 (-05)',
+    imageURL: 'https://myapp.koombea.com/smash/70.png',
+  },
+
+  {
+    objectID: 'f56aa7cd-6c66-4752-bbb7-36fee86bd27d',
+    name: 'Banjo & Kazooie',
+    universe: 'Banjo-Kazooie',
+    price: '760',
+    popular: true,
+    rate: 4,
+    downloads: '39812',
+    description:
+      'Banjo & Kazooie (バンジョー&カズーイ, Banjo & Kazooie), formatted in their reveal trailer as Banjo-Kazooie, are a playable fighter in Super Smash Bros. Ultimate, and are the first third-party fighter from Microsoft. They were revealed alongside Hero during the E3 Nintendo Direct on June 11th, 2019 as the third downloadable fighter from Fighters Pass Vol. 1. Banjo & Kazooie were released as part of Challenger Pack 3 on September 4th, 2019 and are classified as Fighter #73. Chris Sutherland reprises his role as both Banjo & Kazooie, with his portrayals from across the Banjo-Kazooie series being repurposed for Ultimate in all regions.',
+    created_at: 'Sat Jan 29 2022 01:50:52 GMT-0500 (-05)',
+    imageURL: 'https://myapp.koombea.com/smash/82.png',
+  },
+  {
+    objectID: 'f56aa7cd-6c66-4752-bbb7-36fee86bd27d',
+    name: 'Kazuya',
+    universe: 'Tekken',
+    price: '714',
+    popular: true,
+    rate: 4,
+    downloads: '7884',
+    description:
+      "Kazuya (カズヤ, Kazuya) is a playable character in Super Smash Bros. Ultimate. He was officially announced at the E3 Nintendo Direct on June 15th, 2021 as the fifth downloadable character from Fighters Pass Vol. 2 and was released on June 29th, 2021 (June 30th, 2021 in Europe and Asia) as part of Challenger Pack 10. Kazuya is the second playable character from Bandai Namco after Pac-Man and is classified as Fighter #81. Masanori Shinohara, Kazuya's voice actor since Tekken 4, reprises his role in Ultimate in all regions.",
+    created_at: 'Sat Jan 29 2022 01:50:52 GMT-0500 (-05)',
+    imageURL: 'https://myapp.koombea.com/smash/85.png',
+  },
+];
---
+++ src/utils/fighter.utils.test.ts
@@ -0,0 +1,14 @@
+import {templateSettings} from 'lodash';
+import {fightersData} from '../store/fake/fightersData';
+import {getFightersByRate, getFightersSorted} from './fighters.utils';
+
+test('should filter fighters by rate ', () => {
+  const filtered = getFightersByRate(fightersData, 5);
+  expect(filtered.length).toBe(0);
+});
+
+test('should sort fighters by a given prop ', () => {
+  const sorted = getFightersSorted(fightersData, 'price');
+  console.log('sorted ==>', sorted);
+  //   expect(filtered.length).toBe(0);
+});
