9b5ea3e61440e3ae57e8794fe44702586d40a4cb
Andris Ramirez <46964876+aarc97@users.noreply.github.com>
2022-02-08 15:22:29 -0400

    testing fighters filter methods

--- src/utils/fighter.utils.test.ts
+++ src/utils/fighter.utils.test.ts
@@ -1,11 +1,19 @@
 import {fightersData} from '../store/fake/fightersData';
-import {getFightersByRate, getFightersSorted} from './fighters.utils';
+import {getFightersByRate, getFightersFormatted} from './fighters.utils';
 
-test('should filter fighters by rate return 0 ', () => {
+test('should filter fighters by rate 5 and return 0 fighters ', () => {
   const filtered = getFightersByRate(fightersData, 5);
   expect(filtered.length).toBe(0);
 });
 
-test('should sort fighters by a given prop ', () => {
+test('should sort by "name" and filter by "4" rate and return 2 fighters ', () => {
+  const result = getFightersFormatted({
+    data: fightersData,
+    rate: 4,
+    sort: 'name',
+  });
+
+  expect(result.length).toBe(2);
+  expect(result[0].name).toBe('Banjo & Kazooie');
   // const sorted = getFightersSorted(fightersData, 'price');
 });
